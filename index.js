fed68b51700fbc8f0828b207f1b191864eb6a212
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-07-06 20:07:46 +0200

    Consulta de poblaciones

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const {Poblacion} = require('./models')
 
 app.get("/", function (req, res) {
     res.json("hola");
+})
+
+
+app.get("/poblaciones", function (req, res) {
+    Poblacion.findAll()
+    .then(poblaciones => res.json(poblaciones))
+    .catch(err => res.json(err))
 })
 
 
